1d190a9788d9a17d3bb588411fac97eaa05ca87a
kanurys <kanurys@gmail.com>
2016-02-14 18:17:27 -0800

    exit gracefully
    -added gexit() to gracefully exit the program and unexport the enable
    pin
    -cleaned up the comments a bit for ease of reading

--- lib/pi/master.rb
+++ lib/pi/master.rb
@@ -28,7 +28,7 @@ $message = String.new
 length_string = String.new
 $start_of_text = 2.chr
 $end_of_text   = 3.chr
-$device_id     = 254    #Pi IP (SELF)
+$device_id     = 254    								#Pi IP (SELF)
 $seperator     = ':'
 
 $arduinos      = []
@@ -37,7 +37,7 @@ puts "send 'exit' from another device on the rs485 network to close this program
 
 #params for GPIO
 $enable_pin = PiPiper::Pin.new(:pin => 18, :direction => :out)
-$enable_pin.off					#initialize in off state
+$enable_pin.off											#initialize in off state
 
 
 
@@ -50,10 +50,10 @@ def send(recipient, msg)
 	output_buffer << $start_of_text << to_id_string(recipient) << $seperator << to_id_string($device_id) << $seperator << to_msg_length(msg) << $seperator << msg << $end_of_text
 
 	#echo recieved message back to sender
-	$enable_pin.on					  #enable trasmission on RS485 network
-	sleep(0.01)					#allow network to establish "transmit" state
+	$enable_pin.on					  					#enable trasmission on RS485 network
+	sleep(0.01)											#allow network to establish "transmit" state
 	$serial_port.write(output_buffer)
-	$enable_pin.off					#disable trasmission on RS485 network
+	$enable_pin.off										#disable trasmission on RS485 network
 
 	puts "Sent on rs485: " + output_buffer
 end
@@ -86,19 +86,19 @@ end
 #function to read the serial buffer
 def read
 	$message = ""
-	hash = String.new						#this is where the address and length data is stored
+	hash = String.new									#this is where the address and length data is stored
 	if is_message? $serial_port.getbyte()				#getbyte() returns the first byte if it exists or nil if nothing exists
-		sleep(0.06)						#delay needed to receive at least 64 chars on hardware buffer, experimentally derrived
+		sleep(0.06)										#delay needed to receive at least 64 chars on hardware buffer, experimentally derrived
 
-		hash = $serial_port.read(10)				#read in all the hash data
+		hash = $serial_port.read(10)					#read in all the hash data
 			puts hash
 		recipient = hash[0..2].to_i
 		sender = hash[4..6].to_i
 		message_length = hash[8..9].to_i
-		$serial_port.getbyte()					#read in seperator and clear it from the buffer
+		$serial_port.getbyte()							#read in seperator and clear it from the buffer
 
-		$message = $serial_port.read(message_length)		#read in the message
-		analyze_message($message)				#this function only prints stuff out, at this point
+		$message = $serial_port.read(message_length)	#read in the message
+		analyze_message($message)						#this function only prints stuff out, at this point
 	end
 	$message
 end
@@ -115,7 +115,7 @@ def analyze_message(message)
 end
 
 def return_message(message)
-	if $serial_port.getbyte() == $end_of_text #Check end char and return
+	if $serial_port.getbyte() == $end_of_text 			#Check end char and return
 		send($message)
 	end
 end
@@ -124,9 +124,8 @@ end
 
 
 
-#rock the infinite loop, bro
-#while true do
-#	if read == "exit"
-#		exit
-#	end
-#end
+def gexit()												#graceful exit (gexit)
+	$enable_pin.release
+	puts "enable pin released, exiting..."
+	exit
+end
